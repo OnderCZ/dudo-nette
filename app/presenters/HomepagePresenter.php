1c5c353fc14bf52213a68e76f2b26f52e16add4a
OnderCZ <staryondrej@gmail.com>
2017-10-24 12:33:36 +0200

    výpis textů z DB

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -7,4 +7,19 @@ use Nette;
 
 class HomepagePresenter extends Nette\Application\UI\Presenter
 {
+  /**
+   *
+   * @var Nette\Database\Context
+   */
+  private $database;
+
+  public function __construct(Nette\Database\Context $database) {
+    $this->database = $database;
+  }
+
+  public function renderDefault() {
+    $this->template->posts = $this->database->table('web_texty')
+            ->order('created_at DESC');
+            //->limit(5);
+  }
 }
